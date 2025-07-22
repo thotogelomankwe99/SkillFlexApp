665a23ffe43c1a14e50f7f1be0a82b788f73846a
thotogelomankwe99 <thotogelomankwe@gmail.com>
2025-07-22 09:54:40 +0200

    saving local changes before pulling

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//SkillFlexApp's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyBh4dUfZ8m61xsqa0qhG3aFguVZ4Gm2KCQ",
+      authDomain: "skillflexapp.firebaseapp.com",
+      projectId: "skillflexapp",
+      storageBucket: "skillflexapp.firebasestorage.app",
+      messagingSenderId: "827795374450",
+      appId: "1:827795374450:web:4f64fa1bb85c158badddeb",
+      measurementId: "G-PJ701TTVZJ"
+    };
+
+    //Firebase initialization
+    firebase.initializeApp(firebaseConfig);
+
 function resetPassword() {
   const newPassword = document.getElementById("new-password").value;
   const confirmPassword = document.getElementById("confirm-password").value;
