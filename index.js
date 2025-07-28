5606be52a8bc0ad2861a44f201cd3856a74bc287
Rose Melato <rosemelato25@gmail.com>
2025-07-28 11:08:19 +0200

    added firebase authentication for login.

---
+++ index.js
@@ -0,0 +1,7 @@
+//Linking buttons to external html files
+document.getElementById("getStartedBtn").addEventListener("click", function(){
+  window.location.href="sign-up.html";
+});
+document.getElementById("learnMoreBtn").addEventListener("click",function(){
+  window.location.href="about.html";
+});
