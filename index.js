d7bdb6a0eea1ced74a14f443659d2773c664e5f0
Rose Melato <rosemelato25@gmail.com>
2025-07-29 22:59:30 +0200

    added responsiveness for the login page in mobile and tablet devices.

--- index.js
+++ index.js
@@ -6,8 +6,3 @@ document.getElementById("learnMoreBtn").addEventListener("click",function(){
   window.location.href="about.html";
 });
 
-//navigation for mobile and tablet
-function toggleMenu(){
-  const nav =document.querySelector(".header-navigation");
-  nav.classList.toggle("active");
-}
