a048c18a77a2786ffdcb4b683f86eed33cac29be
Rose Melato <rosemelato25@gmail.com>
2025-08-23 21:27:23 +0200

    modified the styling for the learner dashboard and added authentication for login .

---
+++ landing.js
@@ -0,0 +1,8 @@
+//linking buttons to external html files
+document.getElementById("getStartedBtn").addEventListener("click", function()
+{
+    window.location.href="sign-up.html";
+});
+document.getElementById("learnMoreBtn").addEventListener("click", function(){
+    window.location.href="login.html"
+})
